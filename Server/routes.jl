f37996acff15faa2b56f3813189497e579d77bdb
Kanav Gupta <kanav0610@gmail.com>
2020-06-03 15:17:48 +0530

    (api) add API boilerplate

--- Server/routes.jl
+++ Server/routes.jl
@@ -1,5 +1,10 @@
 using Genie.Router
+using RegisterController
 
 route("/") do
   serve_static_file("welcome.html")
 end
+
+route("/register", RegisterController.register_page, method = "GET", named= :register_page)
+
+route("/api/register", RegisterController.register, method = "POST", named = :create_user)

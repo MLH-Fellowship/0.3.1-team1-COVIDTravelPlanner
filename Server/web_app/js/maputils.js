322c205f729724a4bae654b32b038c4af42df57e
LakshyaKhatri <lakshyak67@gmail.com>
2020-06-04 18:35:00 +0530

    Add graph for showing prediction

--- Server/web_app/js/maputils.js
+++ Server/web_app/js/maputils.js
@@ -1,4 +1,5 @@
-function geocode(map, searchService, query){
+
+function predict(map, searchService, query, ){
     searchService.geocode({
         q: query,
     }, (result) => {
@@ -7,15 +8,26 @@ function geocode(map, searchService, query){
             map.removeObjects(map.getObjects())
             map.addObject(new H.map.Marker(item.position));
             map.setCenter(item.position);
-        });
-    }, alert);
+
+            $.get('http://127.0.0.1:8087/api/v1/status', {
+                state: item.address.state,
+                district: item.address.city
+            }, function(data){
+                console.log(data);
+            }).fail(function(){
+                console.log("error");
+            });
+        }, alert);
+    });
 }
 
-var searchBar = document.getElementById('loc-searchbar')
+var searchBar = document.getElementById('loc-searchbar');
 searchBar.addEventListener('keyup', function(event){
     if (event.keyCode === 13) {
         event.preventDefault();
         query = searchBar.value;
-        geocode(map, searchService, query);
+        predict(map, searchService, query);
     }
 })
+
+document.getElementById('sidebar-date').innerHTML = moment().format("DD MMMM YY");

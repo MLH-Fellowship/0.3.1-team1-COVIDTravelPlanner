05ad5c710c57aaec2e0ce00a525c9aadabee74bf
Lakshya Khatri <lakshyak67@gmail.com>
2020-06-04 23:05:30 +0530

    Add a web based application (#10)
    * Add files for webapp
    
    * Add basic map and sidebar
    
    * Add geocoding
    
    * Resolve source map url warning
    
    * Add graph for showing prediction
    
    * Method implementation for prediction graph
    
    * rename web_app => app
    
    * Add range selector and district restrictions

---
+++ Server/app/js/main.js
@@ -0,0 +1,99 @@
+
+function predict(map, searchService, query){
+    searchService.geocode({
+        q: query,
+    }, (result) => {
+        // Add a marker for each location found
+        result.items.forEach((item) => {
+            if(item.address.countryCode != "IND"){
+                alert("Current version supports Indian districts only!");
+                return;
+            }
+            map.removeObjects(map.getObjects())
+            map.addObject(new H.map.Marker(item.position));
+            map.setCenter(item.position);
+
+            $.ajax({
+                type: 'GET',
+                url:'http://127.0.0.1:8081/api/v1/status/',
+                data: {
+                    state: item.address.state,
+                    district: item.address.city
+                },
+                headers: {
+                    'mozSystem': true,
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods':'PUT, GET, POST',
+                    'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept'
+                },
+                success: function(data){
+                    data = JSON.parse(data);
+                    updateData(data);
+                }
+            });
+        }, alert);
+    });
+}
+
+function updateData(data){
+    predictionGraph.data.datasets[0].data = data.infected;
+    predictionGraph.data.datasets[1].data = data.rd;
+    predictionGraph.update();
+    confirmedCases.innerHTML = data.infected[0];
+    recDeaths.innerHTML = data.rd[0];
+}
+
+var searchBar = document.getElementById('loc-searchbar');
+searchBar.addEventListener('keyup', function(event){
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        query = searchBar.value;
+        predict(map, searchService, query);
+    }
+})
+var sidebarDate = document.getElementById('sidebar-date');
+sidebarDate.innerHTML = moment().format("DD MMMM YY");
+var confirmedCases = document.getElementById('confirmed-cases');
+var recDeaths = document.getElementById('recovered-deaths');
+
+// Set up the prediction graph for the first time
+var ctx = document.getElementById('prediction-graph').getContext('2d');
+var x = [];
+for (var i = 0; i < 7; i++) {
+    x[i] = moment().add(i, 'day').format('DD MMM');
+}
+var predictionGraph = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: x,
+        datasets: [{
+            label: 'predicted cases',
+            backgroundColor: [
+                'rgba(153, 102, 255, 0.0)',
+            ],
+            borderColor: [
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 4
+        },
+        {
+            label: 'recovered + deaths',
+            backgroundColor: [
+                'rgba(153, 102, 255, 0.0)',
+            ],
+            borderColor: [
+                'rgba(0, 0, 0, 1)',
+            ],
+            borderWidth: 4
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});

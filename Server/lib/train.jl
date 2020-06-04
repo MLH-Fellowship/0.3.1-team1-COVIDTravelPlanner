e457e2474a4a3b3bccb1d7068c947659c0f5623f
avik-pal <avikpal@iitk.ac.in>
2020-06-04 16:38:40 -0400

    [FIX] Modify loss functions to incorporate recoveries

--- Server/lib/train.jl
+++ Server/lib/train.jl
@@ -3,8 +3,9 @@ Zygote.@nograd update_loss_log
 
 function train(prob, t, u0, p3, ps, data, epochs, opt, avg_meter,
                plen; cont_train = false)
-    log_infected = log.(abs.(data["infected"]))
-    log_recovered = log.(abs.(data["recovered"] .+ data["deceased"]))    
+    log_infected = log.(abs.(data["infected"]) .+ 1.0)
+    log_recovered = log.(abs.(data["recovered"] .+ data["deceased"]) .+ 1.0)
+    weightage = data["time_train"] ./ length(data["time_train"])
     
     run_logs = Dict(
         "loss" => [],
@@ -25,8 +26,8 @@ function train(prob, t, u0, p3, ps, data, epochs, opt, avg_meter,
     
     function loss_adjoint()
         prediction = predict_adjoint()
-        infect_loss = sum(abs2, log_infected .- log.(abs.(prediction[2, :])))
-        recover_loss = 0.0 # sum(abs2, log_recovered .- log.(abs.(prediction[3, :])))
+        infect_loss = 3.0 * sum(weightage .* abs2.(log_infected .- log.(abs.(prediction[2, :]) .+ 1.0)))
+        recover_loss = sum(weightage .* abs2.(log_recovered .- log.(abs.(prediction[3, :]) .+ 1.0)))
         loss = infect_loss + recover_loss
 
         update(avg_meter, loss)
@@ -66,13 +67,21 @@ function inference(prob, u0, p, t)
     return S_NN, I_NN, R_NN, T_NN
 end
 
-function train_all_districts(file; each_epochs = 200, retrain = false)
+function train_named_locations(file::String, districts, states; each_epochs = 200, retrain = false)
     data_all = clean_data(file)
+    train_named_locations(data_all, districts, states; each_epochs = each_epochs, retrain = retrain)
+end
+
+function train_named_locations(data_all, districts, states; each_epochs = 200, retrain = false)
     avg_meter = RunningAverageMeter()
-    for state in keys(data_all)
+    districts = format_string.(districts)
+    for state in format_string.(states)
         data_state = data_all[state]
         @info "Starting to train models for State: $state"
         for district in keys(data_state)
+            if length(districts) > 0 && district ∉ districts
+                continue
+            end
             df = data_state[district]
             if isnothing(df)
                 continue
@@ -85,15 +94,28 @@ function train_all_districts(file; each_epochs = 200, retrain = false)
             @info "District $district"
             t_train = df["time_train"]
             t_test = df["time_test"]
+
+            u0_1 = df["infected"][1]
+            u0_2 = df["recovered"][1] + df["deceased"][1]
             ps, re, u0, prob, p3, plen =
-                construct_model(t_train, t_test, hidden_dim = 30, test_model = false)
-            opt = ADAM(1e-3)
+                construct_model(t_train, t_test, u0_1, u0_2, hidden_dim = 30,
+                                test_model = false)
+            opt = ADAM(1e-2)
+            @show df["infected"], df["recovered"] .+ df["deceased"], df["time_train"]
             u0, p = train(prob, t_train, u0, p3, ps, df, each_epochs, opt, avg_meter, plen,
                           cont_train = false)
+    
+            @show inference(prob, u0, p, t_train)[2:3]
+            @show inference(prob, u0, p, t_test)[2:3]
             BSON.@save filepath u0 p t_train t_test
             @info "Saved model to $filepath"
         end
     end
+end
+
+function train_all_districts(file; each_epochs = 200, retrain = false)
+    data_all = clean_data(file)
+    train_named_locations(data_all, [], collect(keys(data_all)); each_epochs = each_epochs, retrain = retrain)
 end
 
 prediction(state::String, district::String, day_num::Int) =
@@ -105,8 +127,11 @@ function prediction(state::String, district::String, day_num::Vector{Int})
         return nothing
     end
     BSON.@load filepath u0 p t_train t_test
+#     @show t_train, t_test
     _, _, _, prob, _, _ =
-        construct_model(t_train, t_test, hidden_dim = 30, test_model = false)
+        construct_model(t_train, t_test, u0[2], u0[3], hidden_dim = 30, test_model = false)
+    s_nn, i_nn, r_nn, t_nn = inference(prob, u0, p, t_train)
+#     @show i_nn, r_nn
     s_nn, i_nn, r_nn, t_nn = inference(prob, u0, p, t_test)
     return i_nn[day_num], r_nn[day_num]
 end
